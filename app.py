eb574e24a5ebe0caa09e08c9d20c9a9685e449d7
AkshitaSharma211 <sharma.1608akshita@gmail.com>
2026-04-03 10:11:55 +0530

    feat: add custom 404 error page

--- app.py
+++ app.py
@@ -94,5 +94,9 @@ def update(id):
     db.session.commit()
     return redirect("/")
 
+@app.errorhandler(404)
+def page_not_found(e):
+       return render_template("404.html"), 404
+
 if __name__ == "__main__":
    app.run(debug=True,port=4848) 
